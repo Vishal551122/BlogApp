921e8c74c755e919cd9d9c67ff23b93324b41e84
Vishal551122 <vishal6042000@gmail.com>
2024-09-12 18:21:49 +0530

    all work done for this BlogApp

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import appwriteService from '../appwrite/conf'
+import appwriteService from '../appwrite/config'
 import { Link } from 'react-router-dom'
 
 function PostCard({ $id, title, featuredImage}){
--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,28 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup";
+
+import Select from "./Select";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Login from  "./Login";
+import Input from "./Input";
+import Button from "./Button";
 export {
     Header,
     Footer,
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Input,
+    Select,
+    Button
 }
